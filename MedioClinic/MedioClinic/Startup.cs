cdf72c4fc2870d083ff2fcabb873164aa1ad3635
FredBoin <frederic.boin@zeroseven.com.au>
2021-09-10 09:41:49 +1000

    First commit

---
+++ MedioClinic/MedioClinic/Startup.cs
@@ -0,0 +1,127 @@
+using Autofac;
+using Kentico.Content.Web.Mvc;
+using Kentico.Content.Web.Mvc.Routing;
+using Kentico.Web.Mvc;
+using MedioClinic.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+using XperienceAdapter.Localization;
+
+namespace MedioClinic
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+
+        public AutoFacConfig AutoFacConfig => new AutoFacConfig();
+
+
+
+        public Startup(IWebHostEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Enable desired Kentico Xperience features
+            var kenticoServiceCollection = services.AddKentico(features =>
+            {
+                //features.UsePageBuilder();
+                //features.UseActivityTracking();
+                //features.UseABTesting();
+                //features.UseWebAnalytics();
+                //features.UseEmailTracking();
+                //features.UseCampaignLogger();
+                //features.UseScheduler();
+                features.UsePageRouting(new PageRoutingOptions { CultureCodeRouteValuesKey = "culture" });
+            });
+
+            if (Environment.IsDevelopment())
+            {
+                // By default, Xperience sends cookies using SameSite=Lax. If the administration and live site applications
+                // are hosted on separate domains, this ensures cookies are set with SameSite=None and Secure. The configuration
+                // only applies when communicating with the Xperience administration via preview links. Both applications also need 
+                // to use a secure connection (HTTPS) to ensure cookies are not rejected by the client.
+                kenticoServiceCollection.SetAdminCookiesSameSiteNone();
+
+                // By default, Xperience requires a secure connection (HTTPS) if administration and live site applications
+                // are hosted on separate domains. This configuration simplifies the initial setup of the development
+                // or evaluation environment without a the need for secure connection. The system ignores authentication
+                // cookies and this information is taken from the URL.
+                kenticoServiceCollection.DisableVirtualContextSecurityForLocalhost();
+            }
+
+            //services.AddAuthentication();
+            // services.AddAuthorization();
+            services.AddAntiforgery();
+
+            services.AddLocalization();
+
+            services.AddControllersWithViews()
+                .AddDataAnnotationsLocalization(options =>
+                {
+                    options.DataAnnotationLocalizerProvider = (type, factory) =>
+                    {
+                        var assemblyName = typeof(SharedResource).GetTypeInfo().Assembly.GetName().Name;
+
+                        return factory.Create("SharedResource", assemblyName);
+                    };
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStaticFiles();
+
+            app.UseKentico();
+
+            app.UseCookiePolicy();
+
+            app.UseCors();
+
+            //app.UseAuthentication();
+            // app.UseAuthorization();
+
+            app.UseBrowserLink();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.Kentico().MapRoutes();
+
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("The site has not been configured yet.");
+                //});
+            });
+        }
+
+        private void RegisterInitializationHandler(ContainerBuilder builder) =>
+            CMS.Base.ApplicationEvents.Initialized.Execute += (sender, eventArgs) => AutoFacConfig.ConfigureContainer(builder);
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            try
+            {
+                AutoFacConfig.ConfigureContainer(builder);
+            }
+            catch
+            {
+                RegisterInitializationHandler(builder);
+            }
+        }
+    }
+}
